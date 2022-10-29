da48457e89166ec532819201814853cf2f8d7b9e
Panji Anwar Sanusi <panjianwar5@gmail.com>
2022-10-30 04:29:51 +0700

    Menambahkan File:

---
+++ luaslingkaran.py
@@ -0,0 +1,6 @@
+phi = 3.14
+r = float(input("Masukan Panjang Jari-Jari Lingkaran"))
+luas = phi*r*r
+print("Luas Lingkaran Adalah : "+ str(luas))
+k = 2 * phi * r
+print("Keliling Lingkaran Adalah {}".format(k))
